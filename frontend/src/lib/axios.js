e5be4f7630eaa0aea0dfa7b372c0b5a304e53981
Clay0010 <ahmedayad328@gmail.com>
2025-08-04 01:30:09 +0300

    fix: axios setup

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 // const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api" : import.meta.env.VITE_API_URL;
 // const VITE_API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+const BASE_URL =
+  import.meta.env.VITE_BACKEND_URL || "http://localhost:5000/api"; // takes the backend url from render
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
