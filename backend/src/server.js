35d6db5c6a9c3654b4c17520bbb19a8090093007
Clay0010 <ahmedayad328@gmail.com>
2025-08-03 23:34:36 +0300

    before deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,6 +15,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 5000;
+const _dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -25,17 +26,25 @@ app.use(
   })
 );
 
-const swaggerFile = JSON.parse(
-  fs.readFileSync(path.resolve("./swagger.json"), "utf-8")
-);
+// const swaggerFile = JSON.parse(
+//   fs.readFileSync(path.resolve("./swagger.json"), "utf-8")
+// );
 
 app.use("/api/upload", uploadRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(_dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(_dirname, "../frontend/dist/index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-// const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
-const VITE_API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
+// const VITE_API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 
 export const axiosInstance = axios.create({
-    baseURL: VITE_API_URL,
+    baseURL: BASE_URL,
     withCredentials: true,
 })
